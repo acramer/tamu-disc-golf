4c66b05a79c579e606c016fa651a0cd8ddeb392f
ChandlerOchs <chandlerochs@yahoo.com>
2020-10-08 22:54:55 -0500

    Folder structure sucks, but bootstrap is running

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ var app = express()
 var http = require('http')
 var server = http.Server(app)
 
-app.use(express.static('client'))
+app.use('/client', express.static(__dirname + '/client'))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/index.html')
+})
+
+console.log(__dirname)
 
 server.listen(PORT, function() {
     console.log('Server running, congratulations.')
