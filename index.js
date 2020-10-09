cbca785bd08a8ce4427f8f4cf04fc07d197bd50d
ChandlerOchs <chandlerochs@yahoo.com>
2020-10-08 21:42:32 -0500

    Procfile for instructing heroku how to run. index.js for express. npm isntalled express

---
+++ index.js
@@ -0,0 +1,12 @@
+var PORT = process.env.PORT || 3000 //process.env.port is the port for heroku
+var express = require('express')
+var app = express()
+
+var http = require('http')
+var server = http.Server(app)
+
+app.use(express.static('client'))
+
+server.listne(PORT, function() {
+    console.log('Server running, congratulations.')
+})
