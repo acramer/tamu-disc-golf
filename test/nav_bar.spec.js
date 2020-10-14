896d4177a1edaab267e928ab0d871cf7cd331026
acramer <acramerxc@utexas.edu>
2020-10-14 14:25:57 -0500

    added title to navbar on all pages

--- test/nav_bar.spec.js
+++ test/nav_bar.spec.js
@@ -7,7 +7,21 @@ const assert = require('assert');
 
 describe('navigation bar', function() {
   var valid_routes = [
-    {name:'landing',path: '/'                   ,},
+    {name:'landing' ,path: '/'                    ,},
+    {name:'landing' ,path: '/client/index.html'   ,},
+    {name:'about'   ,path: '/client/about.html'   ,},
+    {name:'services',path: '/client/services.html',},
+    {name:'contact' ,path: '/client/contact.html' ,},
+  ];
+
+  var expected_contents = [
+    {name:'Club Name Item'  ,expected: 'TAMU Disc Golf',},
+    {name:'About Item'      ,expected: 'About'         ,},
+    {name:'Services Item'   ,expected: 'Services'      ,},
+    {name:'Contact Item'    ,expected: 'Contact'       ,},
+    {name:'Portfolio Item'  ,expected: 'Portfolio'     ,},
+    {name:'Blog Item'       ,expected: 'Blog'          ,},
+    {name:'Other Pages Item',expected: 'Other Pages'   ,},
   ];
 
   before(function() {
@@ -17,15 +31,15 @@ describe('navigation bar', function() {
 
   valid_routes.forEach(function(route) {
     describe('Checking '+route.name+' page for nav bar',function() {
-        before(function(done) {
-          this.browser.visit(route.path, done);
-        });
-        it(route.name+' page should load', function() {
-          assert.ok(this.browser.success);
-        });
-        it('should exist on the '+route.name+' page', function() {
-          assert(this.browser.text('a.navbar-brand').includes('TAMU Disc Golf'));
-        });
+      before(function(done) {
+        this.browser.visit(route.path, done);
+      });
+      it(route.name+' page should load', function() {
+        assert.ok(this.browser.success);
+      });
+      it('should exist on the '+route.name+' page', function() {
+        assert(this.browser.text('a.navbar-brand').includes('TAMU Disc Golf'));
+      });
     });
 
   })
@@ -38,35 +52,23 @@ describe('navigation bar', function() {
       rawNav = this.browser.html('nav');
       processedNav = rawNav.replace(/</g,'>').split('>').map(function(item) {return item.trim()})
     });
-    it('should contain "TAMU Disc Golf"', function() {
-        assert(processedNav.indexOf('TAMU Disc Golf') > -1)
+    describe('Checking Existance of each item of the navbar', function() {
+      expected_contents.forEach(function(item) {
+        it('should contain "'+item.name+'"', function() {
+            assert(processedNav.indexOf(item.expected) > -1)
+        });
+      });
     });
-    it('should contain "About"', function() {
-        assert(processedNav.indexOf('About') > -1)
-    });
-    it('should contain "Services"', function() {
-        assert(processedNav.indexOf('Services') > -1)
-    });
-    it('should contain "Contact"', function() {
-        assert(processedNav.indexOf('Contact') > -1)
-    });
-    it('should contain "Portfolio"', function() {
-        assert(processedNav.indexOf('Portfolio') > -1)
-    });
-    it('should contain "Blog"', function() {
-        assert(processedNav.indexOf('Blog') > -1)
-    });
-    it('should contain "Other Pages"', function() {
-        assert(processedNav.indexOf('Other Pages') > -1)
-    });
-    it('should contain "TAMU Disc Golf" before "About"', function() {
-        assert(processedNav.indexOf('TAMU Disc Golf') < processedNav.indexOf('About'))
-    });
-    it('should contain "About" before "Services"', function() {
-        assert(processedNav.indexOf('About') < processedNav.indexOf('Services'))
-    });
-    it('should contain "Services" before "Contact"', function() {
-        assert(processedNav.indexOf('Services') < processedNav.indexOf('Contact'))
+    describe('Checking order of each item of the navbar', function() {
+      for (i = 1; i < expected_contents.length; i++) {
+        name1 = expected_contents[i-1].name
+        name2 = expected_contents[i].name
+        val1 = expected_contents[i-1].expected
+        val2 = expected_contents[i].expected
+        it('should contain "'+name1+'" before "'+name2+'"', function() {
+            assert(processedNav.indexOf(val1) < processedNav.indexOf(val2))
+        });
+      }
     });
   });
 
