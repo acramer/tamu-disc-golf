90e19d8d873276a317519a9202203d4574482467
ChandlerOchs <chandlerochs@yahoo.com>
2020-10-24 18:18:35 -0500

    following the heroku postgresql tutorial

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ var http = require('http')
 
 const app = module.exports = express();
 
+/*Database*/
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+/*Database*/
+
 app.use('/client', express.static(__dirname + '/client'))
 
 app.get('/', function(req, res) {
