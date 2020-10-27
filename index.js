6ff3d741ff540e5e81e24584cadcf0d5fd0805df
acramer <acramerxc@utexas.edu>
2020-10-27 11:40:38 -0500

    I did it

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var PORT = process.env.PORT || 2000 //process.env.port is the port for heroku
 var express = require('express')
+var fs = require('fs')
+var path = require('path')
+
 
 var http = require('http')
 
 const app = module.exports = express();
 
-app.use('/client', express.static(__dirname + '/client'))
+app.use(express.static(path.join(__dirname, 'client')))
+app.use(require('./routes/index.js'));
 
-app.get('/', function(req, res) {
-    res.redirect('/client/index.html');
-})
-
-console.log(__dirname)
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/client/index.html');
+// })
+// 
+// app.get('/courses', function(req, res) {
+//     res.sendFile(__dirname + '/client/courses.html');
+// })
+// 
+// app.get('/officers', function(req, res) {
+//     res.sendFile(__dirname + '/client/officers.html');
+// })
+// 
+// app.get('/lostfound', function(req, res) {
+//     res.sendFile(__dirname + '/client/lostfound.html');
+// })
 
 module.exports = app;
 if (!module.parent) {
