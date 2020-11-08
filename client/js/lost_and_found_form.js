f84c20b3f61e340e92761c32d218fdae2340774d
ChandlerOchs <chandlerochs@yahoo.com>
2020-11-08 16:37:17 -0600

    Have a nice looking form modal now

--- client/js/lost_and_found_form.js
+++ client/js/lost_and_found_form.js
@@ -1,4 +1,11 @@
-function LostAndFoundForm() {
-    //create a form element
-    
+function submitLAFForm() {
+    //check the form for it email is fine
+    var result = {}
+    result["email"] = document.getElementById("exampleInputEmail1").value
+    result["phoneNum"] = document.getElementById("LAFPhone").value
+    result["details"] = document.getElementById("LAFDescription").value
+    result["image"] = document.getElementById("LAFImage").value
+    console.log(result) 
+
+    //add the data to the database
 }
