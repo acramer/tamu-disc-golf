7a155bde2c7040b89d57f5ef80a1ca225eca2f5b
ChandlerOchs <chandlerochs@yahoo.com>
2020-11-08 18:25:18 -0600

    I think I have a post request working but I need to be on master to test...

--- client/js/lost_and_found_form.js
+++ client/js/lost_and_found_form.js
@@ -1,11 +1,20 @@
 function submitLAFForm() {
     //check the form for it email is fine
     var result = {}
-    result["email"] = document.getElementById("exampleInputEmail1").value
-    result["phoneNum"] = document.getElementById("LAFPhone").value
-    result["details"] = document.getElementById("LAFDescription").value
-    result["image"] = document.getElementById("LAFImage").value
+    var email = document.getElementById("exampleInputEmail1").value
+    var number = document.getElementById("LAFPhone").value
+    var details = document.getElementById("LAFDescription").value
+    // result["image"] = document.getElementById("LAFImage").value
+    var title = document.getElementById("LAFTitle").value
     console.log(result) 
-
+    const data = {email, number, details, title};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    fetch('/testing', options);
     //add the data to the database
 }
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var path = require('path')
 var http = require('http')
 
 const app = module.exports = express();
+app.use(express.json());
 
 /*Database*/
 const { Pool } = require('pg');
@@ -21,6 +22,21 @@ app.get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.json(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+app.post('/testing', async (req, res) => {
+    console.log('we here')
+    console.log(req.body)
+    try {
+      const client = await pool.connect();
+      const result = await client.query('INSERT INTO users (title, email, details, phone_num) VALUES ($1, $2, $3, $4)', [req.body['title'], req.body['email'], req.body['details'], req.body['number']]);
       const results = { 'results': (result) ? result.rows : null};
       res.json(results);
       client.release();
