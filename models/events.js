7a46a90a391859f7f39bf68b370af9f83cb0acbc
Prathiksha R Prasad <prathiksharprasad@Prathikshas-MacBook-Pro.local>
2020-11-10 01:17:19 -0600

    Migrations for DB

---
+++ models/events.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class events extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  events.init({
+    event_title: DataTypes.TEXT,
+    event_date: DataTypes.DATE,
+    event_place: DataTypes.TEXT,
+    event_description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'events',
+  });
+  return events;
+};
