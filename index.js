7516a6b9059939d956bbcbc31e37850f057c66df
Prathiksha R Prasad <prathiksharprasad@Prathikshas-MacBook-Pro.local>
2020-11-10 02:48:39 -0600

    Added upcomig events section

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ var exphbs  = require('express-handlebars');
 var fs = require('fs')
 var path = require('path')
 var http = require('http')
+var Sequelize = require('sequelize')
 
 const app = module.exports = express();
 app.use(express.json());
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-/*Database*/
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = require('./models/index.js')
+const Op = Sequelize.Op
 
+var helpers = require('handlebars-helpers')();
 
 app.use(express.static(path.join(__dirname, 'views')))
 app.use(require('./routes/index.js'));
