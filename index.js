adcd96425d6608528b1f578137350bc4e6de9971
Snehasish Sen <snehasish.sen@tamu.edu>
2020-11-10 00:28:43 -0600

    Cleaning up client dir

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var fs = require('fs')
 var path = require('path')
 var http = require('http')
 
-
-
 const app = module.exports = express();
 app.use(express.json());
 app.engine('handlebars', exphbs());
@@ -21,96 +19,8 @@ const pool = new Pool({
   }
 });
 
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.json(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
 
-app.post('/testing', async (req, res) => {
-    console.log('we here')
-    console.log(req.body['title'])
-    try {
-        var title = req.body['title']
-        var email = req.body['email']
-        var details = req.body['details']
-        const client = await pool.connect();
-        const result = await client.query('INSERT INTO lost_and_found (title, email, details, phone_num) VALUES ($1, $2, $3, $4)', [title, email, details, req.body['number']]);
-    //   const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.json(results);
-        client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-
-app.get('/laf', async (req, res) => {
-    console.log('we here')
-    console.log(req.body['title'])
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM lost_and_found');
-      const results = { 'results': (result) ? result.rows : null};
-      res.json(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-
-app.get('/events', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * from events WHERE event_date >= current_date ORDER BY event_date LIMIT 3');
-      const results = { 'results': (result) ? result.rows : null};
-      res.json(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-
-  
-app.get('/officers-db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * from officers');
-    const results = { 'results': (result) ? result.rows : null};
-    res.json(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
-
-app.get('/competitive-teams', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * from competitive_teams');
-    const results = { 'results': (result) ? result.rows : null};
-    res.json(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
-
-/*Database*/
-
-app.use(express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, 'views')))
 app.use(require('./routes/index.js'));
 
 module.exports = app;
--- routes/landing.js
+++ routes/landing.js
@@ -1,9 +1,11 @@
 var express = require('express')
+
 var router = express.Router();
 var path = require('path')
 
-router.get( '/', function( req, res, next ) {
-  res.sendFile(path.resolve('client/index.html') ) ;
+router.get( '/', function( req, res, next) {
+  // res.sendFile(path.resolve('client/index.html') ) ;
+  res.render(path.resolve('views/index'), {})
 } ) ;
 
 module.exports = router
