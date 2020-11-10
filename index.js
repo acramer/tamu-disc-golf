5ce0a9fb70da638c614bdbac651eb7b5b828719d
Snehasish Sen <snehasish.sen@tamu.edu>
2020-11-10 00:28:43 -0600

    Cleaning up client dir

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var fs = require('fs')
 var path = require('path')
 var http = require('http')
 
-
-
 const app = module.exports = express();
 app.use(express.json());
 app.engine('handlebars', exphbs());
@@ -21,55 +19,55 @@ const pool = new Pool({
   }
 });
 
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.json(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+// app.get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect();
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.json(results);
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
 
-app.post('/testing', async (req, res) => {
-    console.log('we here')
-    console.log(req.body['title'])
-    try {
-        var title = req.body['title']
-        var email = req.body['email']
-        var details = req.body['details']
-        const client = await pool.connect();
-        const result = await client.query('INSERT INTO lost_and_found (title, email, details, phone_num) VALUES ($1, $2, $3, $4)', [title, email, details, req.body['number']]);
-    //   const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.json(results);
-        client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+// app.post('/testing', async (req, res) => {
+//     console.log('we here')
+//     console.log(req.body['title'])
+//     try {
+//         var title = req.body['title']
+//         var email = req.body['email']
+//         var details = req.body['details']
+//         const client = await pool.connect();
+//         const result = await client.query('INSERT INTO lost_and_found (title, email, details, phone_num) VALUES ($1, $2, $3, $4)', [title, email, details, req.body['number']]);
+//     //   const result = await client.query('SELECT * FROM test_table');
+//         const results = { 'results': (result) ? result.rows : null};
+//         res.json(results);
+//         client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
 
-app.get('/laf', async (req, res) => {
-    console.log('we here')
-    console.log(req.body['title'])
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM lost_and_found');
-      const results = { 'results': (result) ? result.rows : null};
-      res.json(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-/*Database*/
+// app.get('/laf', async (req, res) => {
+//     console.log('we here')
+//     console.log(req.body['title'])
+//     try {
+//       const client = await pool.connect();
+//       const result = await client.query('SELECT * FROM lost_and_found');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.json(results);
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
+// /*Database*/
 
-app.use(express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, 'views')))
 app.use(require('./routes/index.js'));
 
 module.exports = app;
