37fc28d5bf7074c9d068543e61b55bd36e535b1d
Alok Chandrawal <AlokMacbook@AlokMacBook.local>
2020-11-10 21:28:29 -0600

    a bit change

--- index.js
+++ index.js
@@ -77,6 +77,21 @@ app.get('/events', async (req, res) => {
       res.send("Error " + err);
     }
   })
+
+app.get('/teams_info', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * from competitive_teams');
+    const results = { 'results': (result) ? result.rows : null};
+    res.json(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+
 /*Database*/
 
 app.use(express.static(path.join(__dirname, 'client')))
